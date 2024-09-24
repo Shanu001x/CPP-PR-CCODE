80b78929c967f037274a6d6aad467e7f074ff278
Shanu001x <sraw419@gmail.com>
2024-09-24 11:41:40 +0530

    Pattern Solving

--- patterns/01.cpp
+++ patterns/01.cpp
@@ -5,13 +5,16 @@ int main() {
     int count;
     cout<<"How many times you want to run this loop: - ";
     cin>>count;
+    int incr = 1;
     for (int i = 0; i < count; i++)
     {
         
-        for (int j = 0; j < i+1; j++)
+        
+        for (int j = 0; j < i; j++)
         {
             
-         cout<<"+";
+         cout<<incr << " ";
+         incr = incr + 1;
         }
 
         cout<<endl;
